6545c09aeca28f6487bcf44a9e55c9645d2e57db
N3xxus <129999096+FatihIndraa@users.noreply.github.com>
2025-02-04 07:48:45 +0700

    update footer

--- pklLaravel/resources/views/layout/footer.blade.php
+++ pklLaravel/resources/views/layout/footer.blade.php
@@ -1,124 +1,79 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
 <footer class="footer pt-5 mt-5">
     <hr class="horizontal dark mb-5">
     <div class="container">
-        <div class=" row">
+        <div class="row">
+            <!-- Logo / Studio Name -->
             <div class="col-md-3 mb-4 ms-auto">
-                <div>
-                    <h6 class="text-gradient text-primary font-weight-bolder">Soft UI Design 3 System</h6>
-                </div>
-                <div>
-                    <h6 class="mt-3 mb-2 opacity-8">Social</h6>
-                    <ul class="d-flex flex-row ms-n3 nav">
-                        <li class="nav-item">
-                            <a class="nav-link pe-1" href="https://www.facebook.com/CreativeTim/"
-                                target="_blank">
-                                <i class="fab fa-facebook text-lg opacity-8"></i>
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link pe-1" href="https://twitter.com/creativetim" target="_blank">
-                                <i class="fab fa-twitter text-lg opacity-8"></i>
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link pe-1" href="https://dribbble.com/creativetim" target="_blank">
-                                <i class="fab fa-dribbble text-lg opacity-8"></i>
-                            </a>
-                        </li>
-
-
-                        <li class="nav-item">
-                            <a class="nav-link pe-1" href="https://github.com/creativetimofficial"
-                                target="_blank">
-                                <i class="fab fa-github text-lg opacity-8"></i>
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link pe-1"
-                                href="https://www.youtube.com/channel/UCVyTG4sCw-rOvB9oHkzZD1w" target="_blank">
-                                <i class="fab fa-youtube text-lg opacity-8"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+                <h6 class="text-gradient text-primary font-weight-bolder">Studio Foto Delapan Kudus</h6>
+                <p class="opacity-8">Membantu menangkap momen terbaik Anda dengan layanan foto profesional.</p>
             </div>
 
-
-
-            <div class="col-md-2 col-sm-6 col-6 mb-4">
-                <div>
-                    <h6 class="text-gradient text-primary text-sm">Resources</h6>
-                    <ul class="flex-column ms-n3 nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://iradesign.io/" target="_blank">
-                                Illustrations
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.creative-tim.com/bits" target="_blank">
-                                Bits & Snippets
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.creative-tim.com/affiliates/new"
-                                target="_blank">
-                                Affiliate Program
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+            <!-- Social Media Links -->
+            <div class="col-md-3 mb-4">
+                <h6 class="text-gradient text-primary">Ikuti Kami</h6>
+                <ul class="d-flex flex-row ms-n3 nav">
+                    <li class="nav-item">
+                        <a class="nav-link pe-1" href="https://www.facebook.com/studio.fatih" target="_blank">
+                            <i class="fab fa-facebook text-lg opacity-8"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link pe-1" href="https://twitter.com/studio.fatih" target="_blank">
+                            <i class="fab fa-twitter text-lg opacity-8"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link pe-1" href="https://www.instagram.com/studio.fatih" target="_blank">
+                            <i class="fab fa-instagram text-lg opacity-8"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link pe-1" href="https://www.youtube.com/channel/studio.fatih" target="_blank">
+                            <i class="fab fa-youtube text-lg opacity-8"></i>
+                        </a>
+                    </li>
+                </ul>
             </div>
 
-            <div class="col-md-2 col-sm-6 col-6 mb-4">
-                <div>
-                    <h6 class="text-gradient text-primary text-sm">Help & Support</h6>
-                    <ul class="flex-column ms-n3 nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.creative-tim.com/contact-us" target="_blank">
-                                Contact Us
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.creative-tim.com/knowledge-center"
-                                target="_blank">
-                                Knowledge Center
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://services.creative-tim.com/?ref=ct-soft-ui-footer"
-                                target="_blank">
-                                Custom Development
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.creative-tim.com/sponsorships" target="_blank">
-                                Sponsorships
-                            </a>
-                        </li>
-
-                    </ul>
-                </div>
+            <!-- Useful Links -->
+            <div class="col-md-2 mb-4">
+                <h6 class="text-gradient text-primary">Layanan Kami</h6>
+                <ul class="flex-column ms-n3 nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/booking" target="_blank">Pesan Sekarang</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#paket">Harga Paket</a>
+                    </li>
+                </ul>
             </div>
 
-            <div class="col-md-2 col-sm-6 col-6 mb-4 me-auto">
+            <!-- Contact Info -->
+            <div class="col-md-2 mb-4">
+                <h6 class="text-gradient text-primary">Kontak</h6>
+                <ul class="flex-column ms-n3 nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="tel:+62895381191380">Call: +62 895-3811-91380</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://goo.gl/maps/xyz" target="_blank">Alamat: Jl. Dwarawati VIII Perumahan Gerbang Harapan No.8, Gondangmanis, Kec. Bae, Kabupaten Kudus, Jawa Tengah 59327</a>
+                    </li>
+                </ul>
             </div>
 
+            <!-- Empty Column for Layout Adjustment -->
+            <div class="col-md-2 mb-4 me-auto"></div>
+
+            <!-- Footer Copyright -->
             <div class="col-12">
                 <div class="text-center">
                     <p class="my-4 text-sm">
                         All rights reserved. Copyright ©
                         <script>
                             document.write(new Date().getFullYear())
-                        </script> Soft UI Design System by <a href="https://www.creative-tim.com"
-                            target="_blank">Creative Tim</a>.
+                        </script> Studio Foto Fatih.
                     </p>
                 </div>
             </div>
